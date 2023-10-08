f200496f6bd14a075055797506fac6ea41829831
Dev Aryan Dogra <devdogra1@gmail.com>
2023-10-08 14:34:11 +0530

    Connect mongoose to MongoDB DB

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 require('dotenv').config(); 
 const express = require('express'); 
+const mongoose = require('mongoose');
+
 const PORT = process.env.PORT || 3500; 
+const DB_URI = process.env.DB_URI;
+
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`Listening at http://127.0.0.1:${PORT}`)
-})
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect(DB_URI); 
+    console.log("Connected to DB");
+
+    app.listen(PORT, () => {
+        console.log(`Listening at http://127.0.0.1:${PORT}`)
+    })
+
+}
