7ca6bf95fd9c9abf04ca5594ed7e252b0c5909c8
Dev Aryan Dogra <devdogra1@gmail.com>
2023-10-09 12:05:00 +0530

    Make controllers/ + extract registerController.js to it

--- routes/api/register.js
+++ routes/api/register.js
@@ -1,7 +1,5 @@
 const express = require('express'); 
-const ROLES = require("../../config/roles.json");
-const User = require("../../models/User")
-
+const registerController = require("../../controllers/registerController"); 
 const router = express.Router(); 
 
 // DONE: Dont allow registration of accounts with role other than "Customer"
@@ -17,33 +15,6 @@ phone NUMBER 10DIGITS REQD
 role STRING - Customer | Admin | Employee
 */
 router.route('/')
-    .post(async (req, res) => {
-        console.log("Registering..."); 
-        let userData = req.body;
-        if (userData.role && userData.role != ROLES.Customer) {
-            return res.status(401).json({message: "New accounts can only have the 'Customer' role"})
-        }
-
-        const user = new User(userData);
-        try {
-            await user.save();
-            return res.status(201).json({success: "Account created succesfully"}); 
-        } catch(err) {
-            console.log({...err}); 
-            let errorMsg = err.message; 
-            // If there is a duplicate error, find what fields it exists on and set the appropriate errorMsg
-            if (err.code === 11000) {
-                Object.keys(user._doc).forEach(key => {
-                    console.log({key}); 
-                    if (err.keyValue[key]) {
-                        errorMsg = `An account with the provided ${key} already exists`
-                    }
-                })
-            }
-
-            return res.status(400).json({error: errorMsg})
-        }
-
-    })
+    .post(registerController.handleRegister)
 
 module.exports = router; 
