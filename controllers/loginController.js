d7897222c56e5b983f1242845cf7881a5a7f5608
Dev Aryan Dogra <devdogra1@gmail.com>
2023-10-15 17:21:01 +0530

    Send login succesful message on succesful login

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,11 +1,24 @@
+const bcrypt = require('bcrypt'); 
 const User = require('../models/User');
 
 const handleLogin = async (req, res) => {
-    const user = await User.findByPhoneNumber(9811061693);
-    res.status(200).json({
-        message: "Hello this is the /login route",
-        user
-    })
+    const { phone, password } = req.body;
+
+    try {
+        const user = await User.findByPhoneNumber(phone);
+        if (!user) return res.status(404).json({ error: "No user with the given phone number exists" });
+
+        const hashedPassword = user.password; 
+        const matches = await bcrypt.compare(password, hashedPassword);
+        if (!matches) return res.status(401).json({ error: "Incorrect password" });
+
+        // Login successful
+        return res.status(200).json({ success: "Login succesful" });
+    } catch(err) {
+        console.log(err.message); 
+        return res.status(400).json({ error: err.message, message: "Could not login" });
+    }
+    
 }
 
 module.exports = {
