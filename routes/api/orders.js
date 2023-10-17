15265620cc9a79bc698cb89aac5908ba86cd31af
Dev Aryan Dogra <devdogra1@gmail.com>
2023-10-17 23:16:35 +0530

    Protect POST orders/ by role

--- routes/api/orders.js
+++ routes/api/orders.js
@@ -1,10 +1,12 @@
 const express = require('express'); 
 const router = express.Router(); 
 const ordersController = require('../../controllers/ordersController');
+const allowRoles = require('../../middleware/allowRoles');
+const ROLES = require('../../config/roles')
 
 router.route('/')
-    .get(ordersController.getAllOrdersByUserId)
-    .post(ordersController.createOrder)
+    .get(ordersController.getAllOrdersByUserId) // If no ID is supplied, just GETs all orders
+    .post(allowRoles(ROLES.Customer, ROLES.Manager, ROLES.Admin), ordersController.createOrder)
 
 router.route('/:id')
     .get(ordersController.getOrderById)
@@ -13,3 +15,4 @@ router.route('/:id')
 
 
 module.exports = router;
+
