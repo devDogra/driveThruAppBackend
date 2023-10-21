daecbb245c0a2c3a4f89cf9bf233b761dc4804f7
Dev Aryan Dogra <devdogra1@gmail.com>
2023-10-21 23:20:24 +0530

    Restrict MenuItem PUT/POST/DELETE to Managers and above

--- routes/api/menuItems.js
+++ routes/api/menuItems.js
@@ -1,16 +1,29 @@
 const express = require('express'); 
 const router = express.Router(); 
 const menuItemsController = require('../../controllers/menuItemsController'); 
+const ROLES = require('../../config/roles.json');
+const allowRoles = require('../../middleware/allowRoles')
+
+const allowAllRoles = allowRoles(...Object.keys(ROLES));
+
+const allowedRoles = {
+    GET: allowAllRoles,
+    GET_id: allowAllRoles,
+    POST: allowRoles(ROLES.Manager, ROLES.Admin),
+    PUT_id: allowRoles(ROLES.Manager, ROLES.Admin),
+    DELETE_id: allowRoles(ROLES.Manager, ROLES.Admin),
+}
+
 
 router.route('/:id')
-    .get(menuItemsController.getMenuItemById)
-    .delete(menuItemsController.deleteMenuItemById)
-    .put(menuItemsController.updateMenuItemById)
+    .get(allowedRoles.GET_id, menuItemsController.getMenuItemById)
+    .delete(allowedRoles.DELETE_id, menuItemsController.deleteMenuItemById)
+    .put(allowedRoles.PUT_id, menuItemsController.updateMenuItemById)
 
 
 router.route('/')
-    .get(menuItemsController.getAllMenuItems)
-    .post(menuItemsController.createMenuItem)
+    .get(allowedRoles.GET, menuItemsController.getAllMenuItems)
+    .post(allowedRoles.POST, menuItemsController.createMenuItem)
 
 
 module.exports = router; 
