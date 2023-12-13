dce3db8fe3066d367091d542619d78f371586ffe
Dev Aryan Dogra <devdogra1@gmail.com>
2023-12-13 19:27:34 +0530

    Change accessToken expiration time to 12h

--- dbscripts/populateAccounts.js
+++ dbscripts/populateAccounts.js
@@ -2,36 +2,11 @@ const { saltRounds } = require('../config/bcryptConfig.json');
 const mongoose = require('mongoose'); 
 const User = mongoose.model("User"); 
 const ROLES = require("../config/roles.json");
+const sampleUsers = require('../config/sampleUsers');
 const bcrypt = require('bcrypt'); 
 
 module.exports = async function populateAccounts() {
-    const user1 = {
-        firstName: "Dev",
-        phone: 9811061693,
-        password: "devdevdev",
-        role: ROLES.Admin,
-    }
-    const user2 = {
-        firstName: "testMgr",
-        lastName: "testMgr",
-        phone: 9811061692,
-        password: "mgrmgrmgr",
-        role: ROLES.Manager,
-    }
-    const user3 = {
-        firstName: "testEmp",
-        lastName: "testEmp",
-        phone: 9811061691,
-        password: "empempemp",
-        role: ROLES.Employee,
-    };
-    const user4 = {
-        firstName: "testCust",
-        lastName: "testCust",
-        password: "cuscuscus",
-        phone: 9811061690,
-        role: ROLES.Customer,
-    };
+    const {user1, user2, user3, user4 } = sampleUsers;
 
     const hashedPassword1 = await bcrypt.hash(user1.password, saltRounds)
     const hashedPassword2 = await bcrypt.hash(user2.password, saltRounds)
